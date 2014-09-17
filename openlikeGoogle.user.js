0fe33b213aee1f5c569052e69103503500338bc3
Kipkemoi Kiptum <kipkemoi@users.noreply.github.com>
2014-09-17 08:09:08 +0300

    Update openlikeGoogle.user.js

--- openlikeGoogle.user.js
+++ openlikeGoogle.user.js
@@ -5,7 +5,7 @@
 // @description  enter something useful
 // @downloadURL https://github.com/kipkemoi/userscripts/raw/master/openlikeGoogle.user.js
 // @match       *://*.ft.tld/*
-// @match       *://*economist.tld/*
+// @match       *://*.economist.tld/*
 // @user-agent Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)
 // @copyright  2011+, You
 // ==/UserScript==
