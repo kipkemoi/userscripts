bdbe635887566101e1a172b096f8c0ea32a28cd4
Kipkemoi Kiptum <kipkemoi@users.noreply.github.com>
2014-09-17 08:07:19 +0300

    Create openlikeGoogle.user.js

---
+++ openlikeGoogle.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name       UA | FT.com
+// @namespace  http://ft.com/
+// @version    0.1
+// @description  enter something useful
+// @downloadURL https://github.com/kipkemoi/userscripts/raw/master/openlikeGoogle.user.js
+// @match       *://*.ft.tld/*
+// @match       *://*economist.tld/*
+// @user-agent Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)
+// @copyright  2011+, You
+// ==/UserScript==
