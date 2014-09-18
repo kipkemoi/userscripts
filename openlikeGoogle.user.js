f68a1ef52f39bdb57fef4a6b4610c3a82706a70c
Kipkemoi Kiptum <kipkemoi@users.noreply.github.com>
2014-09-18 07:27:13 +0300

    Update openlikeGoogle.user.js

--- openlikeGoogle.user.js
+++ openlikeGoogle.user.js
@@ -2,6 +2,7 @@
 // @name       UA |Be Google
 // @namespace  http://kipkemoi.com/
 // @version    0.1.0.2
+// @author    Kip
 // @description  Some news sites have limits to the number of pages you can get for free - however they allow google to go through them. This changes the User-agent to appear to be googlebot. It works on google chrome using Tampermonkey
 // @downloadURL https://github.com/kipkemoi/userscripts/raw/master/openlikeGoogle.user.js
 // @match       *://*.ft.tld/*
