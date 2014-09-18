6118c007629db41f7283094a89d2ed78cf44e39f
Kipkemoi Kiptum <kipkemoi@users.noreply.github.com>
2014-09-18 07:21:24 +0300

    Update openlikeGoogle.user.js
    Added LA Times

--- openlikeGoogle.user.js
+++ openlikeGoogle.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name       UA |Be Google
 // @namespace  http://kipkemoi.com/
-// @version    0.1.0.1
+// @version    0.1.0.2
 // @description  Some news sites have limits to the number of pages you can get for free - however they allow google to go through them. This changes the User-agent to appear to be googlebot. It works on google chrome using Tampermonkey
 // @downloadURL https://github.com/kipkemoi/userscripts/raw/master/openlikeGoogle.user.js
 // @match       *://*.ft.tld/*
 // @match       *://*.economist.tld/*
-// @match       *://*.nytimes.com/
+// @match       *://*.nytimes.tld/*
+// @match       *://*.latimes.tld/*
 // @user-agent Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)
 // @copyright  2014+, Kip
 // ==/UserScript==
